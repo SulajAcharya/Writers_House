e82100262256c219b8d3dccdec525d8b7bcbb616
Ajay7616 <u.ajaykumar7616@gmail.com>
2023-07-17 20:47:19 +0530

    Sql added

--- sql/writers_house.sql
+++ sql/writers_house.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jul 16, 2023 at 02:23 PM
+-- Generation Time: Jul 17, 2023 at 05:14 PM
 -- Server version: 10.4.24-MariaDB
 -- PHP Version: 8.1.6
 
@@ -26,56 +26,250 @@ SET time_zone = "+00:00";
 --
 -- Table structure for table `chat`
 --
--- Error reading structure for table writers_house.chat: #1932 - Table &#039;writers_house.chat&#039; doesn&#039;t exist in engine
--- Error reading data for table writers_house.chat: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `writers_house`.`chat`&#039; at line 1
+
+CREATE TABLE `chat` (
+  `id` bigint(20) NOT NULL,
+  `incoming_id` varchar(255) NOT NULL,
+  `outgoing_id` varchar(255) NOT NULL,
+  `msg` text CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `chat`
+--
+
+INSERT INTO `chat` (`id`, `incoming_id`, `outgoing_id`, `msg`) VALUES
+(1, '9790y7', '75w7677q6', 'Testing');
 
 -- --------------------------------------------------------
 
 --
 -- Table structure for table `comments`
 --
--- Error reading structure for table writers_house.comments: #1932 - Table &#039;writers_house.comments&#039; doesn&#039;t exist in engine
--- Error reading data for table writers_house.comments: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `writers_house`.`comments`&#039; at line 1
+
+CREATE TABLE `comments` (
+  `id` bigint(20) NOT NULL,
+  `comment` text CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
+  `user_id` bigint(20) NOT NULL,
+  `content_id` bigint(20) NOT NULL,
+  `comment_time` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `comments`
+--
+
+INSERT INTO `comments` (`id`, `comment`, `user_id`, `content_id`, `comment_time`) VALUES
+(1, 'Hello', 1, 1, '2023-07-17 15:02:49');
 
 -- --------------------------------------------------------
 
 --
 -- Table structure for table `content`
 --
--- Error reading structure for table writers_house.content: #1932 - Table &#039;writers_house.content&#039; doesn&#039;t exist in engine
--- Error reading data for table writers_house.content: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `writers_house`.`content`&#039; at line 1
+
+CREATE TABLE `content` (
+  `content_id` bigint(20) NOT NULL,
+  `user_id` bigint(20) NOT NULL,
+  `title` varchar(255) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
+  `one_liner` text CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
+  `genre_id` bigint(20) NOT NULL,
+  `writers_content` text CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
+  `cover_img` text CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
+  `read_count` bigint(20) NOT NULL,
+  `likes` bigint(20) NOT NULL,
+  `approved` enum('-1','0','1') CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL DEFAULT '0',
+  `deleted` enum('0','1') CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL DEFAULT '0',
+  `deleted_by` varchar(255) CHARACTER SET utf8 COLLATE utf8_unicode_ci DEFAULT NULL,
+  `disapprove_time` timestamp NULL DEFAULT NULL,
+  `created_time` timestamp NOT NULL DEFAULT current_timestamp(),
+  `modified_time` timestamp NULL DEFAULT NULL,
+  `deleted_time` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `content`
+--
+
+INSERT INTO `content` (`content_id`, `user_id`, `title`, `one_liner`, `genre_id`, `writers_content`, `cover_img`, `read_count`, `likes`, `approved`, `deleted`, `deleted_by`, `disapprove_time`, `created_time`, `modified_time`, `deleted_time`) VALUES
+(1, 1, 'Demo', 'testing', 1, 'Nothing', '', 0, 0, '0', '0', NULL, NULL, '2023-07-17 14:57:04', NULL, NULL);
 
 -- --------------------------------------------------------
 
 --
 -- Table structure for table `feedback`
 --
--- Error reading structure for table writers_house.feedback: #1932 - Table &#039;writers_house.feedback&#039; doesn&#039;t exist in engine
--- Error reading data for table writers_house.feedback: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `writers_house`.`feedback`&#039; at line 1
+
+CREATE TABLE `feedback` (
+  `id` bigint(20) NOT NULL,
+  `email_address` varchar(255) NOT NULL,
+  `feddback_comment` varchar(255) NOT NULL,
+  `rating` enum('great','satisfaction','poor') NOT NULL,
+  `submitted_time` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `feedback`
+--
+
+INSERT INTO `feedback` (`id`, `email_address`, `feddback_comment`, `rating`, `submitted_time`) VALUES
+(1, 'ak@gmail.com', 'Testing', 'great', '2023-07-17 15:14:07');
 
 -- --------------------------------------------------------
 
 --
 -- Table structure for table `genre`
 --
--- Error reading structure for table writers_house.genre: #1932 - Table &#039;writers_house.genre&#039; doesn&#039;t exist in engine
--- Error reading data for table writers_house.genre: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `writers_house`.`genre`&#039; at line 1
+
+CREATE TABLE `genre` (
+  `genre_id` bigint(20) NOT NULL,
+  `name` varchar(255) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
+  `deactivate` enum('0','1') CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL DEFAULT '0',
+  `deleted` enum('0','1') CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL DEFAULT '0',
+  `created_time` timestamp NOT NULL DEFAULT current_timestamp(),
+  `modified_time` timestamp NULL DEFAULT NULL,
+  `deleted_time` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `genre`
+--
+
+INSERT INTO `genre` (`genre_id`, `name`, `deactivate`, `deleted`, `created_time`, `modified_time`, `deleted_time`) VALUES
+(1, 'Horror', '0', '0', '2023-07-17 15:00:34', NULL, NULL);
 
 -- --------------------------------------------------------
 
 --
 -- Table structure for table `previous_visit`
 --
--- Error reading structure for table writers_house.previous_visit: #1932 - Table &#039;writers_house.previous_visit&#039; doesn&#039;t exist in engine
--- Error reading data for table writers_house.previous_visit: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `writers_house`.`previous_visit`&#039; at line 1
+
+CREATE TABLE `previous_visit` (
+  `id` bigint(255) NOT NULL,
+  `user_id` bigint(255) NOT NULL,
+  `content_id` bigint(255) NOT NULL,
+  `removed` enum('0','1') NOT NULL DEFAULT '0',
+  `visited_time` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `previous_visit`
+--
+
+INSERT INTO `previous_visit` (`id`, `user_id`, `content_id`, `removed`, `visited_time`) VALUES
+(1, 1, 1, '0', '2023-07-17 15:07:40');
 
 -- --------------------------------------------------------
 
 --
 -- Table structure for table `user`
 --
--- Error reading structure for table writers_house.user: #1932 - Table &#039;writers_house.user&#039; doesn&#039;t exist in engine
--- Error reading data for table writers_house.user: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `writers_house`.`user`&#039; at line 1
+
+CREATE TABLE `user` (
+  `user_id` bigint(20) NOT NULL,
+  `fname` varchar(255) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
+  `lname` varchar(255) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
+  `user_name` varchar(255) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
+  `email_address` varchar(255) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
+  `password` varchar(255) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
+  `role` enum('admin','writer','user') NOT NULL DEFAULT 'user',
+  `block` enum('0','1') NOT NULL DEFAULT '0',
+  `unique_id` varchar(255) NOT NULL,
+  `img` text NOT NULL,
+  `verified` enum('0','1') NOT NULL DEFAULT '0',
+  `verified_time` timestamp NULL DEFAULT NULL,
+  `last_login_time` timestamp NULL DEFAULT NULL,
+  `created_time` timestamp NOT NULL DEFAULT current_timestamp(),
+  `modified_time` timestamp NULL DEFAULT NULL,
+  `deleted_time` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `user`
+--
+
+INSERT INTO `user` (`user_id`, `fname`, `lname`, `user_name`, `email_address`, `password`, `role`, `block`, `unique_id`, `img`, `verified`, `verified_time`, `last_login_time`, `created_time`, `modified_time`, `deleted_time`) VALUES
+(1, 'Ajay', 'Kumar', 'ak', 'ak@gmail.com', '123', 'user', '0', '', '', '0', NULL, NULL, '2023-07-17 14:49:06', NULL, NULL);
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `chat`
+--
+ALTER TABLE `chat`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `comments`
+--
+ALTER TABLE `comments`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `content`
+--
+ALTER TABLE `content`
+  ADD PRIMARY KEY (`content_id`);
+
+--
+-- Indexes for table `feedback`
+--
+ALTER TABLE `feedback`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `genre`
+--
+ALTER TABLE `genre`
+  ADD PRIMARY KEY (`genre_id`);
+
+--
+-- Indexes for table `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`user_id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `chat`
+--
+ALTER TABLE `chat`
+  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `comments`
+--
+ALTER TABLE `comments`
+  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `content`
+--
+ALTER TABLE `content`
+  MODIFY `content_id` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `feedback`
+--
+ALTER TABLE `feedback`
+  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `genre`
+--
+ALTER TABLE `genre`
+  MODIFY `genre_id` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `user`
+--
+ALTER TABLE `user`
+  MODIFY `user_id` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
