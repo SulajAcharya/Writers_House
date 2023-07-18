632013b69fdaf06e7f2f92bd0ce8faaa5c28652d
Ajay7616 <u.ajaykumar7616@gmail.com>
2023-07-18 18:34:41 +0530

    Chat work started

---
+++ chat/index.php
@@ -0,0 +1,88 @@
+<?php
+    $page_title = "Messages";
+    require_once($_SERVER["DOCUMENT_ROOT"]."/includes/init.php");
+?>
+
+<div class="container" id="message">
+    <div class="row">
+        <div class="col-md-6 offset-md-3">
+            <div class="search">
+                <input type="text" placeholder="Enter an users to search" class="mt-3 w-100 p-3">
+            </div>
+            <div class="card mt-4">
+                <div class="card-body">
+                    <div class="profile my-4">
+                        <div class="row">
+                            <div class="col-md-3">
+                                <img src="" alt="">
+                            </div>
+                            <div class="col-md-9">
+                                <a class="card-title h5 text-decoration-none text-dark">Ajay Kumar</a>
+                                <div class="card-text">You:Hi</div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="profile my-4">
+                        <div class="row">
+                            <div class="col-md-3">
+                                <img src="" alt="">
+                            </div>
+                            <div class="col-md-9">
+                                <a class="card-title h5 text-decoration-none text-dark">Ajay Kumar</a>
+                                <div class="card-text">You:Hi</div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="profile my-4">
+                        <div class="row">
+                            <div class="col-md-3">
+                                <img src="" alt="">
+                            </div>
+                            <div class="col-md-9">
+                                <a class="card-title h5 text-decoration-none text-dark">Ajay Kumar</a>
+                                <div class="card-text">You:Hi</div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="profile my-4">
+                        <div class="row">
+                            <div class="col-md-3">
+                                <img src="" alt="">
+                            </div>
+                            <div class="col-md-9">
+                                <a class="card-title h5 text-decoration-none text-dark">Ajay Kumar</a>
+                                <div class="card-text">You:Hi</div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="profile my-4">
+                        <div class="row">
+                            <div class="col-md-3">
+                                <img src="" alt="">
+                            </div>
+                            <div class="col-md-9">
+                                <a class="card-title h5 text-decoration-none text-dark">Ajay Kumar</a>
+                                <div class="card-text">You:Hi</div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="profile my-4">
+                        <div class="row">
+                            <div class="col-md-3">
+                                <img src="" alt="">
+                            </div>
+                            <div class="col-md-9">
+                                <a class="card-title h5 text-decoration-none text-dark">Ajay Kumar</a>
+                                <div class="card-text">You:Hi</div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+<?php
+    require_once($_SERVER["DOCUMENT_ROOT"]."/includes/footer.php");
+?>
--- includes/header.php
+++ includes/header.php
@@ -5,7 +5,7 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<?php if (isset($page_title) && !empty($page_title)): ?>
 		<title>
-			<?php echo trim($page_title); ?> | Tutors
+			<?php echo trim($page_title); ?> | WritersHouse
 		</title>
 	<?php else: ?>
 		<title>WritersHouse</title>
