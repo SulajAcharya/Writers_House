9930f8402336d6a57c121142230e12ea3784305d
Ajay7616 <u.ajaykumar7616@gmail.com>
2023-07-22 19:00:30 +0530

    Chat ui updated

--- chat/chat.php
+++ chat/chat.php
@@ -12,12 +12,18 @@
                     <img src="" alt="" class="col-md-1">
                     <p class="h4">Ajay Kumar</p>    
                 </div>
-                <div class="card-body">
+                <div class="chat-box card-body">
                     
                 </div>
-                <div class="mx-5">
-                    <input type="text">
-                    <button class="btn btn-primary"><span><i class="fab fa-telegram-plane"></i></span></button>
+                <div class="card-footer d-flex justify-content-start align-items-center p-3">
+                    <div class="input-group">
+                        <form action="#" class="typing-area" autocomplete="off">
+                            <input type="text" name="outgoing_id" value="" hidden>
+                            <input type="text" name="incoming_id" value="" hidden>
+                            <input type="text" class="form-control" name="message" class="input-field" placeholder="Type here">
+                            <span class="btn btn-primary"><i class="fab fa-telegram-plane"></i></span>
+                        </form>
+                    </div>
                 </div>
             </div>
         </div>
