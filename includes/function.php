167b29d4b0488dd4be62c03c5adf38006f92cc42
Ajay7616 <u.ajaykumar7616@gmail.com>
2023-07-24 18:49:51 +0530

    Login function added

--- includes/function.php
+++ includes/function.php
@@ -21,4 +21,30 @@
 		require_once($_SERVER["DOCUMENT_ROOT"]."/includes/error_message.php");
 	}
 
+	function signin($data)
+	{
+		global $db;
+		extract($data);
+		$email_address = htmlspecialchars(trim($email_address));
+		$password = create_hash($password);
+
+		$sql = "SELECT user_id, role FROM user WHERE email_address = :email_address OR user_name = :user_name AND password = :password";
+		$stmt = $db->prepare($sql);
+		$stmt->bindParam(':email_address', $email_address, PDO::PARAM_STR);
+		$stmt->bindParam(':user_name', $user_name, PDO::PARAM_STR);
+		$stmt->bindParam(':password',$password, PDO::PARAM_STR);
+
+		if($stmt->execute())
+		{
+			if($stmt->rowCount() > 0)
+			{
+				$result = $stmt->fetch(PDO::FETCH_ASSOC);
+				$_SESSION["user_id"] = $result["user_id"];
+				$_SESSION["role"] = $result["role"];
+				return true;
+			}
+		}
+		$_SESSION["error_message"][] = "Invalid Login";
+		return false;
+	}
 ?>
--- login/index.php
+++ login/index.php
@@ -1,6 +1,18 @@
 <?php
     $page_title = "Login";
     require_once($_SERVER["DOCUMENT_ROOT"]."/includes/init.php");
+    
+    if(isset($_SESSION["user_id"]) && !empty($_SESSION["user_id"]) && is_numeric($_SESSION["user_id"]) && isset($_SESSION["role"]))
+    {
+		echo "<script>window.location='/".$_SESSION["role"]."';</script>";
+		exit(0);
+	}
+
+    if($_POST["signin"])
+    {
+        signin($_POST);
+        redirect_to_current_page();
+    }
 ?>
 
 <div class="container">
@@ -12,7 +24,7 @@
                     <h2 class="mt-5 mb-3 ms-5 fw-bold text-primary">Sign In</h2>
                     <p class="mb-5 ms-5">Sign in to your account</p>
                     <div class="mb-4 mx-5">
-                        <input type="email" class="form-control me-5" id="email_address" name="email_address" placeholder="Email or Username" required>
+                        <input type="text" class="form-control me-5" id="email_address user_name" name="email_address user_name" placeholder="Email or Username" required>
                     </div>
                     <div class="mb-4 mx-5">
                         <input type="password" class="form-control" id="password" name="password" placeholder="Password" required>
