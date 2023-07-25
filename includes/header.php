4c5a15f5a5b027f2681a2fd99e7eafa08f4cd238
SulajAcharya <139706131+SulajAcharya@users.noreply.github.com>
2023-07-25 19:42:44 +0530

    Website

--- includes/header.php
+++ includes/header.php
@@ -15,3 +15,37 @@
 	<link rel="stylesheet" href="/assets/css/style.css">
 </head>
 <body>
+<nav class="navbar navbar-expand-lg navbar-light bg-light shadow navbar-custom">
+    <div class="container">
+      <a class="navbar-brand writershouse-title font-weight-bold" href="#" style="color: #2091F9;">WritersHouse</a>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav ms-auto"> <!-- Right align the navigation links -->
+          <li class="nav-item">
+            <form class="d-flex">
+              <div class="input-group rounded">
+                <input class="form-control border-end-0 border rounded-start" type="search" placeholder="search" id="example-search-input">
+                <span class="input-group-append">
+                  <button class="btn btn-outline-secondary bg-white border-start-0 border-bottom-0 border rounded-end" type="button">
+                    <i class="fa fa-search"></i>
+                  </button>
+                </span>
+              </div>
+            </form>
+          </li>
+          <li class="nav-item active">
+            <a class="nav-link" href="#"><span class="font-weight-bold">Explore Now</span></a>
+          </li>
+          <li class="nav-item active">
+            <a class="nav-link" href="#"><span class="font-weight-bold">Sign In</span></a>
+          </li>
+          <li class="nav-item active">
+            <a class="nav-link" href="#"><span class="font-weight-bold">Register</span></a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
