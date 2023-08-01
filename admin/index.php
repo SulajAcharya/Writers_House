4d355f4fb0039ef8f01a9551b7b2fa4ffbfae522
Ajay7616 <u.ajaykumar7616@gmail.com>
2023-08-01 18:11:19 +0530

    Admin dashboard added

--- admin/index.php
+++ admin/index.php
@@ -3,6 +3,65 @@
     require_once($_SERVER["DOCUMENT_ROOT"]."/admin/nav.php");
 ?>
 
+<div class="container" id="admin">
+    <div class="row">
+        <p class="h1 fw-bold my-5">Welcome Admin</p>
+        <div class="row">
+            <div class="col-md-4">
+                <div class="card">
+                    <div class="card-body">
+                        <div class="row">
+                            <div class="col-md-6">
+                                <p class="text-center h3 fw-bold">User</p>
+                                <i class="fa fa-user h1 d-flex justify-content-center"></i>
+                            </div>
+                            <div class="col-md-6">
+                                <div class="data d-flex align-items-center justify-content-center h1 fw-bold my-4">
+                                    4
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-4">
+                <div class="card">
+                    <div class="card-body">
+                        <div class="row">
+                            <div class="col-md-6">
+                                <p class="text-center h3 fw-bold">Writer</p>
+                                <i class="fas fa-user-edit h1 d-flex justify-content-center"></i>
+                            </div>
+                            <div class="col-md-6">
+                                <div class="data d-flex align-items-center justify-content-center h1 fw-bold my-4">
+                                    4
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-4">
+                <div class="card">
+                    <div class="card-body">
+                        <div class="row">
+                            <div class="col-md-6">
+                                <p class="text-center h3 fw-bold">Content</p>
+                                <i class="fa fa-scroll h1 d-flex justify-content-center"></i>
+                            </div>
+                            <div class="col-md-6">
+                                <div class="data d-flex align-items-center justify-content-center h1 fw-bold my-4">
+                                    4
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
 <?php
     require_once($_SERVER["DOCUMENT_ROOT"]."/includes/init.php");
 ?>
