45d60adf26dcfcb98d290a8fea20804ddc225468
Ajay7616 <u.ajaykumar7616@gmail.com>
2023-08-12 15:08:39 +0530

    Explore page modified

---
+++ explore.php
@@ -0,0 +1,148 @@
+<?php
+    $page_title = "Explore Page";
+    require_once($_SERVER["DOCUMENT_ROOT"]."/includes/init.php");
+?>
+
+<div class="container mb-5" id="explore">
+    <div class="row">
+        <div class="card mt-5 shadow pb-3 bg-body rounded">
+            <div class="card-body m-3">
+                <h2 class="mt-3 mb-4 fw-bold text-primary">Genre</h2>                 
+                <div class="container">
+                    <div class="row mb-4">
+                        <div class="col-md-3">
+                            <a href="" class="text-decoration-none">
+                                <div class="card-body shadow rounded action">
+                                    <div class="card-body">
+                                        <div class="card-title pt-4 mt-5">Hello</div>
+                                    </div>
+                                </div>
+                            </a>
+                        </div>
+                        <div class="col-md-3">
+                            <div class="card shadow rounded">
+                                <img class="img-fluid rounded" src="assets/img/adventure_bg.png" alt="Genre 2">
+                            </div>
+                        </div>
+                        <div class="col-md-3">
+                            <div class="card shadow rounded">
+                                <img class="img-fluid rounded" src="assets/img/article_png.png" alt="Genre 3">
+                            </div>
+                        </div>
+                        <div class="col-md-3">
+                            <div class="card shadow rounded">
+                                <img class="img-fluid rounded" src="assets/img/crime_bg.png" alt="Genre 4">
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="row mb-4">
+                        <div class="col-md-3">
+                            <div class="card shadow rounded">
+                                <img class="img-fluid rounded" src="assets/img/drama_bg.png" alt="Genre 5">
+                            </div>
+                        </div>
+                        <div class="col-md-3">
+                            <div class="card shadow rounded">
+                                <img class="img-fluid rounded" src="assets/img/fantasy_bg.png" alt="Genre 6">
+                            </div>
+                        </div>
+                        <div class="col-md-3">
+                            <div class="card shadow rounded">
+                                <img class="img-fluid rounded" src="assets/img/fiction_bg.png" alt="Genre 7">
+                            </div>
+                        </div>
+                        <div class="col-md-3">
+                            <div class="card shadow rounded">
+                                <img class="img-fluid rounded" src="assets/img/historic_bg.png" alt="Genre 8">
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="row mb-4">
+                        <div class="col-md-3">
+                            <div class="card shadow rounded">
+                                <img class="img-fluid rounded" src="assets/img/historicfiction_bg.png" alt="Genre 9">
+                            </div>
+                        </div>
+                        <div class="col-md-3">
+                            <div class="card shadow rounded">
+                                <img class="img-fluid rounded" src="assets/img/horror_bg.png" alt="Genre 10">
+                            </div>
+                        </div>
+                        <div class="col-md-3">
+                            <div class="card shadow rounded">
+                                <img class="img-fluid rounded" src="assets/img/humor_bg.png" alt="Genre 11">
+                            </div>
+                        </div>
+                        <div class="col-md-3">
+                            <div class="card shadow rounded">
+                                <img class="img-fluid rounded" src="assets/img/investigation_bg.png" alt="Genre 12">
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="row mb-4">
+                        <div class="col-md-3">
+                            <div class="card shadow rounded">
+                                <img class="img-fluid rounded" src="assets/img/life.png" alt="Genre 13">
+                            </div>
+                        </div>
+                        <div class="col-md-3">
+                            <div class="card shadow rounded">
+                                <img class="img-fluid rounded" src="assets/img/mystery_bg.png" alt="Genre 14">
+                            </div>
+                        </div>
+                        <div class="col-md-3">
+                            <div class="card shadow rounded rounded">
+                                <img class="img-fluid rounded" src="assets/img/nonfiction_bg.png" alt="Genre 15">
+                            </div>
+                        </div>
+                        <div class="col-md-3">
+                            <div class="card shadow rounded">
+                                <img class="img-fluid rounded" src="assets/img/poetry_bg.png" alt="Genre 16">
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="row mb-4">
+                        <div class="col-md-3">
+                            <div class="card shadow rounded">
+                                <img class="img-fluid rounded" src="assets/img/romance_png.png" alt="Genre 17">
+                            </div>
+                        </div>
+                        <div class="col-md-3">
+                            <div class="card shadow rounded">
+                                <img class="img-fluid rounded" src="assets/img/science_fiction.png" alt="Genre 18">
+                            </div>
+                        </div>
+                        <div class="col-md-3">
+                            <div class="card shadow rounded">
+                                <img class="img-fluid rounded" src="assets/img/shortstory_bg.png" alt="Genre 19">
+                            </div>
+                        </div>
+                        <div class="col-md-3">
+                            <div class="card shadow rounded">
+                                <img class="img-fluid rounded" src="assets/img/superhero_bg.png" alt="Genre 20">
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="row">
+                        <div class="col-md-3">
+                            <div class="card shadow rounded">
+                                <img class="img-fluid rounded" src="assets/img/thriller_bg.png" alt="Genre 21">
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
+
+            </div>
+        </div>
+    </div>
+</div>
+
+<?php
+    require_once($_SERVER["DOCUMENT_ROOT"]."/includes/footer.php");
+?>
